a4313d82a6b8274d57f7d7249872b177e978aa6a
Stefan Frede <stefan@frede.dev>
2020-09-24 20:03:21 +0200

    chore: configure webpack-plugin-serve

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,11 @@ const { mode } = require('webpack-nano/argv');
 
 const { MiniHtmlWebpackPlugin } = require('mini-html-webpack-plugin');
 const WebpackBar = require('webpackbar');
+const { WebpackPluginServe } = require('webpack-plugin-serve');
 
 module.exports = {
+  watch: mode === 'development',
+  entry: ['./src', 'webpack-plugin-serve/client'],
   mode,
   plugins: [
     new MiniHtmlWebpackPlugin({
@@ -12,5 +15,11 @@ module.exports = {
       },
     }),
     new WebpackBar(),
+    new WebpackPluginServe({
+      port: process.env.PORT || 8080,
+      static: './dist',
+      liveReload: true,
+      waitForBuild: true,
+    }),
   ],
 };
