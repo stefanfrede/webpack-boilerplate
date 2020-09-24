82a1f37f932a87e6da4e2f93cdf511ae5d67cd5b
Stefan Frede <stefan@frede.dev>
2020-09-24 19:45:36 +0200

    chore: configure mini-html-webpack-plugin

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const { mode } = require('webpack-nano/argv');
+
+const { MiniHtmlWebpackPlugin } = require('mini-html-webpack-plugin');
+
+module.exports = {
+  mode,
+  plugins: [
+    new MiniHtmlWebpackPlugin({
+      context: {
+        title: 'Webpack demo',
+      },
+    }),
+  ],
+};
