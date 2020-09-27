a6edbb195a9fffc8a5f77d0abb48726dae65baa0
Stefan Frede <stefan@frede.dev>
2020-09-27 19:12:33 +0200

    chore: configure code splitting

--- src/component.js
+++ src/component.js
@@ -1,8 +1,18 @@
-export default (text = 'Hello world') => {
+export default (text = 'Hello world!') => {
   const element = document.createElement('div');
 
-  element.className = 'rounded bg-red-100 border max-w-md m-4 p-4';
+  element.className =
+    'rounded bg-gray-100 border-2 border-gray-900 max-w-md m-4 p-4';
   element.textContent = text;
+
+  element.onclick = () =>
+    import('./lazy')
+      .then((lazy) => {
+        element.textContent = lazy.default;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
 
   return element;
 };
