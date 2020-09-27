72e75ddf8bf00229361fa8a8d0c018426676237d
Stefan Frede <stefan@frede.dev>
2020-09-27 20:45:09 +0200

    chore: add a performance budget

--- webpack.config.js
+++ webpack.config.js
@@ -88,6 +88,11 @@ const productionConfig = merge([
         name: 'runtime',
       },
     },
+    performance: {
+      hints: 'warning', // "error" or false are valid too
+      maxEntrypointSize: 50000, // in bytes, default 250k
+      maxAssetSize: 100000, // in bytes
+    },
   },
   parts.attachRevision(),
 ]);
